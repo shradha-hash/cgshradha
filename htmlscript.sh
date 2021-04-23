abc5ae87d75681e774133286c23ef385e55663a9
root <shraddhasalunkhe3@gmail.com>
2021-04-23 10:04:05 +0000

    adding htmlscript

---
+++ htmlscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum install httpd
+echo "<h1> This webpage has been built with the help of Jenkins Server</h1> > /var/www/html/index.html
+systemctl start httpd
+systemctl status httpd
+sleep 10
+
